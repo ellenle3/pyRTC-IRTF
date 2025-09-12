bbae1459e384e3ee7eb479855c3d31f69b91473a
imaka <irtf_rtc>
2025-09-12 13:25:26 -1000

    updated AndorWFS to call WaitForAcquisition instead of continuously polling to see if exposure is done. frame rate appears to be consistent now, but occassional seg fault after many thousands of frames

--- IRTF/test_andor.py
+++ IRTF/test_andor.py
@@ -5,7 +5,8 @@ import numpy as np
 
 from pyRTC.hardware import AndorWFS
 
-FILE = open("/home/felix/src/pyrtc/IRTF/output.txt", "a")
+#FILE = open("/home/felix/src/pyrtc/IRTF/output.txt", "a")
+FILE = open("/home/imaka/asm/pyrtc/pyRTC-IRTF/IRTF/output.txt", "a")
 
 def test_exposure(wfs):
     wfs.expose()
@@ -19,15 +20,20 @@ if __name__ == "__main__":
         conf = utils.read_yaml_file("config_felix.yaml")
         wfs_conf = conf["wfs"]
         wfs = AndorWFS(wfs_conf)
-
+        test_exposure(wfs)
+        #raise RuntimeError("Test complete")
+        #wfs.expose()
         k = 0
-        while wfs.total_frames < 10:
-            #FILE.write(f"{k}\n")
+        Nframes = 10000
+        start = time.time()
+        while k < Nframes:
+            t1 = time.time()
             wfs.expose()
-            FILE.write(f"{wfs.total_frames}\n")
-            #time.sleep(0.1)
+            t2 = time.time()
+            FILE.write(f"{wfs.oldTotalFrames} {t2 - t1}\n")
             k += 1
-
+        end = time.time()
+        FILE.write(f"Frame rate: {Nframes / (end - start)}\n")
         #wfs.setRoi((64, 64, 189, 319))  # Example ROI
         #test_exposure(wfs)
 
